f3070f3532b97b9b8882fd67696b9a764273e664
Gabriel Reis Miranda <grmiranda97@gmail.com>
2023-03-27 18:52:34 -0300

    🚨 Add and fixing Lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,26 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
-      ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
+      files: ['cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+  rules: {
+    'vue/no-multiple-template-root': 0,
+    'vue/multi-word-component-names': 0,
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, semi: false, endOfLine: 'auto' },
+    ],
+  },
 }
